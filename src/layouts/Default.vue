8fc648b486107145f67dd7b68223fea51722b7c1
okakyo <0622okakyo@gmail.com>
2019-11-16 17:28:15 +0900

    デザイン一部変更

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,20 +1,7 @@
 <template lang="pug">
   v-app
     .layout
-      Headers
-      v-navigation-drawer(clipped app v-model="drawer" :temporary="$vuetify.breakpoint.mdAndDown")
-        v-list
-            v-list-item
-                v-list-item-content
-                    v-list-item-title.title 岡　恭平
-                    v-list-item-subtitle John@yahoo.com
-                v-list-item-action
-                    v-icon mdi-menu-down
-            v-divider
-            v-list(nav dense)
-                v-list-item(link v-for="header in headersUrl" :to="header.url" :key="header.title")
-                    v-list-item-content
-                        v-list-item-title(v-text="header.title")
+      Header
             
       v-content
         v-container

8f883202903a928e9e445eb84b9b0face0e5f26b
okakyo <0622okakyo@gmail.com>
2020-01-11 12:33:02 +0900

    sitemap追加　

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,7 +1,6 @@
 <template lang="pug">
   Layout
     v-container
-      v-layout.pa-2(row)
         h1 ブログサイト構築中
         p 現在、 ブログの設定がまだ構築中であるため、未公開です。
 
---
+++ src/pages/tag.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+    blog-base
+ZZ
+</template>
+
+<script>
+    import Vue from 'vue'
+    export default Vue.extend({
+        name: "tag"
+    })
+</script>
+
+<style scoped>
+
+</style>
